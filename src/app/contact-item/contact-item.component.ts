503303cf364e2d9d92ec3a2aef60397229db0d97
Alex Shaposhnikov <alexxforcode@gmail.com>
2022-04-30 11:37:20 +0300

    working app without saving to the store

---
+++ src/app/contact-item/contact-item.component.ts
@@ -0,0 +1,22 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ContactsService} from "../contacts.service";
+
+@Component({
+  selector: 'app-contact-item',
+  templateUrl: './contact-item.component.html',
+  styleUrls: ['./contact-item.component.scss']
+})
+export class ContactItemComponent {
+  @Input() title?: string
+  @Input() value?: string
+  @Input() id?: number
+  @Input() contactId?: number
+  @Output() setEnable: EventEmitter<boolean> = new EventEmitter<boolean>()
+
+  constructor(public contactsService: ContactsService) { }
+
+  deleteInfo() {
+    this.contactsService.deleteContactInfo(this.contactId!, this.id!)
+    this.setEnable.emit(false)
+  }
+}
---
+++ src/app/contacts.service.ts
@@ -0,0 +1,102 @@
+import {Injectable} from "@angular/core";
+
+export interface Info {
+    infoTitle: string
+    infoValue: string
+    id: number
+}
+
+export interface Contact {
+    name: string
+    id: number
+    contactInfo: Info[]
+}
+
+type flagType = 'added' | 'deleted' | null
+
+export interface Buffer {
+    flag: flagType
+    contactId: number
+    index: number
+    info: Info | null
+}
+
+@Injectable({providedIn: 'root'})
+export class ContactsService {
+    stepBackBuffer: Buffer = {
+        flag: null,
+        contactId: 0,
+        index: 0,
+        info: null
+    }
+    contacts: Contact[] = []
+    contactId: number = 0
+
+    changeValuesInStepBackBuffer(flag: flagType, contactsId: number, index: number, info: Info) {
+        this.stepBackBuffer.flag = flag
+        this.stepBackBuffer.contactId = contactsId
+        this.stepBackBuffer.index = index
+        this.stepBackBuffer.info = info
+    }
+
+    addContact(name: string): void {
+        this.contactId++
+        this.contacts.unshift({name: name, id: this.contactId, contactInfo: []})
+    }
+
+    deleteContact(id: number): void {
+        this.contacts = this.contacts.filter((contact) => contact.id != id)
+        if (this.contacts.length == 0) {
+            this.contactId = 0
+        }
+    }
+
+    addContactInfo(infoName: string, infoValue: string, contact: Contact): void {
+        for (let i = 0; i < this.contacts.length; i++){
+            if (this.contacts[i].id == contact.id) {
+                this.contacts[i].contactInfo.push({
+                    infoTitle: infoName,
+                    infoValue,
+                    id: Math.random()
+                })
+
+                this.changeValuesInStepBackBuffer(
+                    'added',
+                    this.contacts[i].id,
+                    this.contacts[i].contactInfo.length - 1,
+                    this.contacts[i].contactInfo[this.contacts[i].contactInfo.length - 1]
+                )
+            }
+        }
+    }
+
+    deleteContactInfo(contactId: number, infoId: number): void {
+        for (let i = 0; i < this.contacts.length; i++) {
+            if (this.contacts[i].id == contactId) {
+                for (let j = 0; j < this.contacts[i].contactInfo.length; j++) {
+                    if (this.contacts[i].contactInfo[j].id == infoId) {
+                      this.changeValuesInStepBackBuffer('deleted', contactId, j, this.contacts[i].contactInfo[j])
+                      this.contacts[i].contactInfo.splice(j, 1)
+                    }
+                }
+            }
+        }
+    }
+
+    getById(id: number): Contact | undefined {
+        return this.contacts.find(c => c.id === id)
+    }
+
+
+
+    takeStepBack(): void {
+        if (this.stepBackBuffer.flag == 'added') {
+            this.getById(this.stepBackBuffer.contactId)!.contactInfo?.splice(this.stepBackBuffer.index, 1)
+        }
+
+        if (this.stepBackBuffer.flag == 'deleted') {
+            this.getById(this.stepBackBuffer.contactId)!.contactInfo?.splice(this.stepBackBuffer.index, 0, this.stepBackBuffer.info!)
+        }
+    }
+}
+
